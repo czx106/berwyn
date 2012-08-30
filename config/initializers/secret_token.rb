064f62e87d5d8411ac9e5201abaf1c178e7314f6
Jesse Taylor <jtayloriit@gmail.com>
2012-08-30 01:02:59 -0500

    Installed Ruby on Rails application skeleton
    This is the default Ruby on Rails installation, except for a modified
    .gitignore file, based on the RailsApps project .gitignore file.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Berwyn::Application.config.secret_token = '06e17ff4e56ff8a946cfd8d4fb914fc5383de29a6f0cedf50e2faf43bfee5c3ba480cdc90b7cb16b79503c55a42bf71a4fbb3b5a6170a630cbcfe03d30f70274'
